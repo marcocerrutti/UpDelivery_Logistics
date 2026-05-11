d485fbd52db57c8c2a1224f192d4e79e93666787
Uchenna Nwaigwe <hermessuffice@gmail.com>
2026-05-11 01:08:08 +0100

    End of walking skeleton for .NET API updated -2

--- UpDelivery.Logistics.API/Controllers/LogisticsOrdersController.cs
+++ UpDelivery.Logistics.API/Controllers/LogisticsOrdersController.cs
@@ -27,6 +27,41 @@ namespace UpDelivery.Logistics.API.Controllers
             return order;
         }
 
-        
+        [HttpPost]
+        public async Task<ActionResult<LogisticsOrder>> CreateOrder(LogisticsOrder order)
+        {
+            context.LogisticsOrders.Add(order);
+            await context.SaveChangesAsync();
+            return order;
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> UpdateOrder(Guid id, LogisticsOrder order)
+        {
+            if(order.Id != id || !OrderExists(id)) return BadRequest("can not update order with id: {id}");
+
+            context.Entry(order).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteOrder(Guid id)
+        {
+            var order = await context.LogisticsOrders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            context.LogisticsOrders.Remove(order);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool OrderExists(Guid id)
+        {
+            return context.LogisticsOrders.Any(e => e.Id == id);
+        }
     }
 }
